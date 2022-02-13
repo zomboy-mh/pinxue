ecfba42804276b51942fef2957b7111fa4416de3
Mikey <32127684@qq.com>
2022-02-13 22:57:11 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+      <div class="text"></div>
+  </div>
+</template>
+
+<style lang="scss">
+.text{
+    width: 100px;
+    height: 100px;
+    background: #f00;
+  transform: translate(100px,100px);
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import fastclick from 'fastclick'
+import './assets/css/base.scss'
+
+//解决移动端100-300ms延迟问题
+fastclick.attach(document.body)
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
