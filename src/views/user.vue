97534ab9cc353a67164ade6d9da01b29b9cebb2f
Zomboy <32127684@qq.com>
2022-06-06 17:33:25 +0800

    2022/6/6

--- src/views/user.vue
+++ src/views/user.vue
@@ -8,7 +8,14 @@
       Zomboy
     </div>
   </div>
-  <div >
+  <div class="centre">
+      <div class="centreLeft">
+        <div class="userCentreName">闪聘进度</div>
+        <img class="userCentreRightImg" src="../assets/images/userCentreRight.png" alt="">
+      </div>
+      <div class="centreRight"></div>
+  </div>
+  <div class="bottom">
 
   </div>
 </div>
@@ -23,11 +30,14 @@ export default {
 <style scoped lang="scss">
   @import "../assets/css/variable";
   @import "../assets/css/mixin";
+  .user{
+    background: #f5f6f7;
+  }
 .header{
   width: 100%;
   height:260px;
   background: url("../assets/images/02.jpg") no-repeat;
-  background-size: 100% 80%;
+  background-size: 100% 100%;
   display: flex;
   .headerImg{
     margin-top: 30px;
@@ -46,4 +56,42 @@ export default {
     @include font_size($font_large);
   }
 }
+  .centre{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 90%;
+    height: 150px;
+    padding: 30px 5% 30px 5%;
+    background: #FFFFFF;
+    .centreLeft{
+      width: 50%;
+      height: 100%;
+      border: 1px solid #000;
+      position: relative;
+      overflow: hidden;
+      .userCentreRightImg{
+        position: absolute;
+        right: -20px;
+        bottom: -30px;
+        width: 160px;
+        height: 160px;
+        transform:rotate(20deg);
+      }
+    }
+    .centreRight{
+      width: 50%;
+      height: 100%;
+      border: 1px solid #000;
+      border-left-color: #FFFFFF;
+    }
+    /*background: blue;*/
+
+  }
+  .bottom{
+    margin-top: 20px;
+    width: 100%;
+    height: 200px;
+    background: #ffff;
+  }
 </style>
